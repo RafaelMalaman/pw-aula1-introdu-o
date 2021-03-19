dd7b5282d3ff83a98bee49e93bbef60d6339d97d
RafaelMalaman <rafael.pfleger@etec.sp.gov.br>
2021-03-19 21:00:13 +0000

    Aula 01 - Primeiro Componente

---
+++ fundamentos01/src/components/basico/OlaMundo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default () => {
+
+    const informacao = "Primeiro Componente JSX"
+
+    return(
+        <>
+        <h2>
+            Componente OlaMundo
+        </h2>
+        <p>
+            {informacao}
+        </p>
+        </>
+    )
+
+}
